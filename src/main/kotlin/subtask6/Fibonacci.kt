f8576ba22997dad4ffcbf58db8ca4cfc9e07414d
Sergei Tsarik <sergei.tsarik@thomsonreuters.com>
2020-04-14 15:50:33 +0300

    done: subtask6 Fibonacci

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -2,8 +2,27 @@ package subtask6
 
 class Fibonacci {
 
-    // TODO: Complete the following function
-    fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+    fun productFibonacciSequenceFor(inputNumber: Int): IntArray {
+        if (inputNumber < 0) {
+            throw NotImplementedError("Not valid input number") as Throwable
+        }
+        if (inputNumber == 0) {
+            return intArrayOf(0, 1, 1);
+        }
+
+        var fibonacciNumbers = mutableListOf<Int>(0, 1)
+        var index = 1
+        var prod : Int
+
+        do {
+            fibonacciNumbers.add(fibonacciNumbers.get(index) + fibonacciNumbers.get(index-1));
+            index += 1;
+            prod = fibonacciNumbers.get(index) * fibonacciNumbers.get(index-1)
+            if (prod == inputNumber) {
+                return intArrayOf(fibonacciNumbers.get(index - 1), fibonacciNumbers.get(index), 1)
+            }
+        } while (prod < inputNumber)
+
+        return intArrayOf(fibonacciNumbers.get(index - 1), fibonacciNumbers.get(index), 0);
     }
 }
