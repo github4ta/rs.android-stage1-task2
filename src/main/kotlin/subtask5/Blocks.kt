02bb741e7fa195eb2618a231ebe24738dbca212d
Sergei Tsarik <sergei.tsarik@thomsonreuters.com>
2020-04-14 15:49:39 +0300

    done: subtask5 Blocks

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,46 @@
 package subtask5
 
+import java.time.LocalDate
+import java.time.format.DateTimeParseException
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        var blockC = mutableListOf<Any>()
+        if (blockA.size <= 0) {
+            throw NotImplementedError("Array is empty")
+        }
+
+        for (i in 0..blockA.size - 1) {
+            if (blockB.isInstance(blockA[i])) {
+                blockC.add(blockA[i])
+            }
+        }
+
+        if (blockB.equals(Int::class)) {
+            return blockC.sumBy { it.toString().toInt() }
+        }
+
+        if (blockB.equals(String::class)) {
+            return blockC.joinToString("") { it.toString() }
+
+        }
+
+        if (blockB.equals(LocalDate::class)) {
+            var blockC2 = mutableListOf<LocalDate>()
+            for (i in 0..blockC.size - 1) {
+                var date = blockC.get(i).toString()
+                try {
+                    blockC2.add(LocalDate.parse(date))
+                } catch (e: DateTimeParseException){
+                    throw NotImplementedError("Date is not correct")
+                }
+            }
+            val maxDate = blockC2.max()
+            return "${maxDate?.dayOfMonth}.${maxDate?.monthValue}.${maxDate?.year}"
+        }
+
+        return throw NotImplementedError("KClass<> is not Int or String, or LocalDate")
     }
 }
