31149253d57b44e893a3fca65e0964cb114b731e
Sergei Tsarik <sergei.tsarik@thomsonreuters.com>
2020-04-14 15:47:08 +0300

    done: subtask1 Date Formatter

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,21 @@
 package subtask1
 
+import java.time.DateTimeException
+import java.time.LocalDate
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val russianMonths = arrayListOf<String>("января", "февраля", "марта", "апреля", "мая", "июня",
+            "июля", "августа", "сентября", "октября", "ноября", "декабря")
+        val russianDayOfWeek = arrayListOf<String>("понедельник", "вторник", "среда", "четверг",
+            "пятница", "суббота", "воскресенье")
+
+        try {
+            val dayOfWeek = LocalDate.of(year.toInt(), month.toInt(), day.toInt()).dayOfWeek.value
+            return "${day} ${russianMonths[month.toInt() - 1]}, ${russianDayOfWeek[dayOfWeek - 1]}"
+        } catch (e: DateTimeException) {
+            return "Такого дня не существует"
+        }
     }
 }
