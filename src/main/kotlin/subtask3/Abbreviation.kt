800201fec02225b55c37b6011f22d3b5ba277323
Sergei Tsarik <sergei.tsarik@thomsonreuters.com>
2020-04-14 15:48:33 +0300

    done: subtask3 Abbreviation

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -2,8 +2,33 @@ package subtask3
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        if(a.length < b.length) {
+            return "NO"
+        }
+
+        var matched = mutableListOf<Char>()
+        val aUppercase = a.toUpperCase()
+        var matcher: Char = b[0]
+        var index = 0
+
+        for (i in 0..aUppercase.length-1) {
+            if (matcher == aUppercase[i]) {
+                matched.add(matcher)
+                index +=1
+                if(index >= b.length) {
+                    break
+                } else {
+                    matcher = b[index]
+                }
+            }
+        }
+
+        val matchedString = matched.joinToString("")
+
+        if (b.equals(matchedString)) {
+            return "YES"
+        }
+        return "NO";
     }
 }
